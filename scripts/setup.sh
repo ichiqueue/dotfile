d467549ccf04019651a75e87cb2dd4856a1b98d0
ichiqueue <ichiqueue@gmail.com>
2016-02-28 21:31:49 +0900

    Create install script

---
+++ scripts/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+set -e
+ln -s $HOME/config/.Xresources $HOME/.Xresources
+ln -s $HOME/config/.Xresources.d $HOME/.Xresources.d
+ln -s $HOME/config/.config $HOME/.config
+ln -s $HOME/config/.xinitrc $HOME/.xinitrc
